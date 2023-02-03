0ed1cfe0db404eecd8c411373ec083182e46a80c
Ahmed Elsheikh <golf17@gmail.com>
2023-02-03 22:47:21 +0200

    Initial commit

--- main.py
+++ main.py
@@ -1,12 +1,17 @@
-from turtle import Screen, Turtle
+from turtle import Screen
 import time
 from snake import Snake
+from food import Food
+from scoreboard import ScoreBoard
 screen = Screen()
 screen.setup(width=600, height=600)
 screen.bgcolor("black")
 screen.title("My Snake Game")
 screen.tracer(0)
 snake = Snake()
+food = Food()
+scoreboard = ScoreBoard()
+
 
 screen.listen()
 screen.onkey(fun=snake.up, key="Up")
@@ -19,11 +24,9 @@ while game_is_on:
     screen.update()
     time.sleep(0.1)
     snake.move()
-
-
-
-
-
-
+    #detect collision with food
+    if snake.head.distance(food) < 15:
+        food.refresh()
+        scoreboard.increase_score()
 
 screen.exitonclick()
