4fb602cdc70b26d1c8d3076f3882097c1ac0d924
Ahmed Elsheikh <golf17@gmail.com>
2023-02-04 01:42:51 +0200

    Initial commit

--- snake.py
+++ snake.py
@@ -1,5 +1,7 @@
 from turtle import Turtle
+# Positions for first 3 segments that refer to squares of snake
 STARTING_POSITION = [(0, 0), (-20, 0), (-40, 0)]
+# forward distance for the snake object
 MOVE_DISTANCE = 20
 UP = 90
 DOWN = 270
@@ -16,11 +18,18 @@ class Snake:
 
     def create_snake(self):
         for position in STARTING_POSITION:
-            new_segment = Turtle(shape="square")
-            new_segment.color("white")
-            new_segment.penup()
-            new_segment.goto(position)
-            self.segments.append(new_segment)
+            self.add_segment(position)
+
+    def add_segment(self, position):
+        new_segment = Turtle(shape="square")
+        new_segment.color("white")
+        new_segment.penup()
+        new_segment.goto(position)
+        self.segments.append(new_segment)
+
+    def extend(self):
+        # add a new segment to the snake
+        self.add_segment()
 
     def move(self):
         for seg_num in range(len(self.segments) - 1, 0, -1):
